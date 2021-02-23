33fe355d4572774b5932fdae26b0a5928ac8a087
klein <allan.klein@epitech.eu>
2021-02-23 09:52:18 +0100

    a terminer

---
+++ circularArrayRotation.js
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/circular-array-rotation/problem
+
+const prisoners = 7 
+const sweets = 19
+const start = 2
+
+function circularArrayRotation(a, k, queries) {
+
+
+}
+
+console.log(
+  circularArrayRotation(a, k, queries)
+)
