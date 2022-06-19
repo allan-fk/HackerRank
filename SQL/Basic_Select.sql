723a1b6a61a86bd1fa14ea9b1a59c046ac7aef28
klein <allan.klein@epitech.eu>
2022-06-19 14:10:15 +0200

    Weather Observation Station 3

--- SQL/Basic_Select.sql
+++ SQL/Basic_Select.sql
@@ -51,3 +51,11 @@ SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN'
 /* Query a list of CITY and STATE from the STATION table.    */
 
 SELECT CITY, STATE FROM STATION
+
+/*----------------------------------------------------------------------------*/
+
+-- Weather Observation Station 3
+
+/* Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.  */
+
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0
