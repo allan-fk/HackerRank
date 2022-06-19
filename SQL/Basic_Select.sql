38104dff7f7864a55d7625eb3b485638cafdfa54
klein <allan.klein@epitech.eu>
2022-06-19 14:24:20 +0200

    Weather Observation Station 4

--- SQL/Basic_Select.sql
+++ SQL/Basic_Select.sql
@@ -59,3 +59,11 @@ SELECT CITY, STATE FROM STATION
 /* Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.  */
 
 SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0
+
+/*----------------------------------------------------------------------------*/
+
+-- Weather Observation Station 4
+
+/* Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. */
+
+SELECT (COUNT(CITY) - COUNT(DISTINCT CITY)) FROM STATION
