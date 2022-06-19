66b8f823d3b629565f80633512bfa974d07a6bc3
klein <allan.klein@epitech.eu>
2022-06-19 15:32:24 +0200

    Weather Observation Station 5

--- SQL/Basic_Select.sql
+++ SQL/Basic_Select.sql
@@ -67,3 +67,16 @@ SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0
 /* Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. */
 
 SELECT (COUNT(CITY) - COUNT(DISTINCT CITY)) FROM STATION
+
+/*----------------------------------------------------------------------------*/
+
+-- Weather Observation Station 5
+
+/* Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.  */
+
+SELECT CITY, length(CITY) LengthCity
+FROM STATION order by LengthCity ASC, CITY ASC
+LIMIT 1;
+SELECT CITY, length(CITY) LengthCity
+FROM STATION order by LengthCity DESC, CITY ASC 
+LIMIT 1;
