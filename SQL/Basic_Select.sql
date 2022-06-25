81ee10f563bc553883216cbfad6eed16db3423d6
klein <allan.klein@epitech.eu>
2022-06-25 23:49:55 +0200

    Weather Observation Station 10

--- SQL/Basic_Select.sql
+++ SQL/Basic_Select.sql
@@ -112,3 +112,13 @@ SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP "^[aeiou].+[aeiou]$"
 /* Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates. */
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[^aeiou]'
+
+/*----------------------------------------------------------------------------*/
+
+-- Weather Observation Station 10
+
+/* Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '[aeiou]$'
+
+
