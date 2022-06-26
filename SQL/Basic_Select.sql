85483f4bef630955eddbcec591abd1504d51ff48
klein <allan.klein@epitech.eu>
2022-06-26 23:55:43 +0200

    Weather Observation Station 11

--- SQL/Basic_Select.sql
+++ SQL/Basic_Select.sql
@@ -121,4 +121,10 @@ SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[^aeiou]'
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '[aeiou]$'
 
+/*----------------------------------------------------------------------------*/
 
+-- Weather Observation Station 11
+
+/* Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates. */
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[AEIOU]' OR CITY NOT REGEXP '[AEIOU]$'
